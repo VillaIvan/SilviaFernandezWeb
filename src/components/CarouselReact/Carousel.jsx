0553fde88dfa48f3856de543a920e12594350858
Ivan Villa <villaivandev@gmail.com>
2024-06-11 17:10:04 -0300

    whatsapp icon, imagenes organizacion de contenido

--- src/components/CarouselReact/Carousel.jsx
+++ src/components/CarouselReact/Carousel.jsx
@@ -14,7 +14,7 @@ import { Keyboard, Pagination, Navigation } from "swiper/modules";
 
 export default function App() {
   return (
-    <div className="h-[85vh] w-[100%]  md:h-[100vh]  animate-fade-in-down  mx-auto ">
+    <div className="h-[85vh] w-[100%]  md:h-[85vh]  animate-fade-in-down  mx-auto">
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -29,7 +29,7 @@ export default function App() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="fondo1 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8  rounded-xl ">
+          <div className="fondo1 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8   ">
             <div className="absolute inset-0 bg-black bg-opacity-50 " />
             <div className="w-[80%] md:w-[65%] lg:w-[700px] xl:w-[700px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0  rounded-xl p-5  z-10">
               {/* <h3 className="text-xl text-secondary text-center md:text-start">
@@ -40,14 +40,14 @@ export default function App() {
                 years.
               </h1>
 
-              <button className=" w-44 px-[2rem] bg-primary text-lg p-2 text-white mx-auto font-semibold hover:bg-white hover:text-primary rounded-md ">
-                Shop Now
+              <button className=" w-[100px] md:w-[30%] px-[2rem] bg-primary text-lg p-3 text-white mx-auto font-semibold hover:bg-white hover:text-primary rounded-md ">
+                COTIZÁ AHORA
               </button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="fondo2 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-end md:pr-24 md:h-[100%] my-auto gap-8  rounded-xl ">
+          <div className="fondo2 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-end md:pr-24 md:h-[100%] my-auto gap-8   ">
             <div className="w-[80%] md:w-[55%] xl:w-[35%] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 bg-white bg-opacity-60 rounded-xl p-5 shadow-md">
               <h3 className="text-xl text-oscure text-center md:text-start">
                 More Luxury and trending
@@ -64,7 +64,7 @@ export default function App() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="fondo3 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-start md:pl-24 md:h-[100%] my-auto gap-8  rounded-xl ">
+          <div className="fondo3 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-start md:pl-24 md:h-[100%] my-auto gap-8   ">
             <div className="w-[80%] md:w-[55%] xl:w-[35%] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 bg-white bg-opacity-60 rounded-xl p-5 shadow-md z-10">
               <h3 className="text-xl text-oscure text-center md:text-start">
                 More Luxury and trending
@@ -81,7 +81,7 @@ export default function App() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="fondo4 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-start md:pl-24 md:h-[100%] my-auto gap-8  rounded-xl ">
+          <div className="fondo4 parallax image relative flex flex-col md:flex-row items-center justify-center md:justify-start md:pl-24 md:h-[100%] my-auto gap-8  ">
             <div className="w-[80%] md:w-[55%] xl:w-[35%] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 bg-white bg-opacity-60 rounded-xl p-5 shadow-md">
               <h3 className="text-xl text-oscure text-center md:text-start">
                 More Luxury and trending
--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -53,7 +53,7 @@ export const ServiceData = [
 
 const ActiveSlider = () => {
   return (
-    <div className=" my-20">
+    <div className=" my-20 landing-section" data-header-color="#0F7261">
       <Swiper
         breakpoints={{
           "@0.75": {
@@ -65,7 +65,7 @@ const ActiveSlider = () => {
           "@1.50": {
             slidesPerView: 2,
           },
-          "@1.750": {
+          "@1.75": {
             slidesPerView: 3,
           },
         }}
@@ -74,19 +74,25 @@ const ActiveSlider = () => {
           clickable: true,
         }}
         modules={[FreeMode]}
-        className="max-w-[100%] lg:max-w-[80%]  "
+        className="max-w-[100%] lg:max-w-[90%]  "
       >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-            <div className="flex flex-col mx-auto group relative shadow-lg text-white rounded-xl px-6 py-8 h-[550px] w-[350px] md:h-[400px]  md:w-[400px] overflow-hidden cursor-pointer">
+            <div className="flex flex-col mx-auto group relative shadow-lg text-white rounded-xl px-6 py-8 h-[550px] w-[350px] md:h-[400px] md:w-[400px] overflow-hidden cursor-pointer">
               <div
-                className="absolute inset-0 bg-cover bg-center "
+                className="absolute inset-0 bg-cover bg-center"
                 style={{ backgroundImage: `url(${item.backgroundImage})` }}
               />
-              <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-0 transition-opacity duration-300" />
-              <div className="relative flex flex-col h-full justify-center items-center gap-3 opacity-100 group-hover:opacity-0 transition-opacity duration-300">
-                <h1 className="text-5xl font-semibold">{item.title} </h1>
-                <p className="lg:text-[18px]">{item.content} </p>
+              <div className="absolute inset-0 bg-black bg-opacity-50" />
+              <div className="relative flex flex-col h-full justify-center items-center gap-3">
+                <div className="absolute flex flex-col justify-center items-center inset-0 opacity-100 group-hover:opacity-0 group-hover:invisible transition-opacity duration-300 ease-in-out">
+                  <h1 className="text-5xl font-semibold text-center">
+                    {item.title}
+                  </h1>
+                </div>
+                <div className="absolute flex flex-col justify-center items-center inset-0 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300 ease-in-out">
+                  <p className="lg:text-[18px] text-center">{item.content}</p>
+                </div>
               </div>
             </div>
           </SwiperSlide>
