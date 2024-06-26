583d7b359030a760d05bee9f1f0bf8b3544f0742
Ivan Villa <villaivandev@gmail.com>
2024-06-26 18:44:02 -0300

    slider modificado, todas las secciones conectadas, cambio de logo smg, titulos de secciones y animaciones

--- src/components/CarouselReact/Carousel.jsx
+++ src/components/CarouselReact/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,6 +13,31 @@ import "./styles.css";
 import { Keyboard, Pagination, Navigation } from "swiper/modules";
 
 export default function App() {
+  useEffect(() => {
+    const handleScroll = (event) => {
+      event.preventDefault();
+      document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+    };
+
+    const slider1Button = document.getElementById("slider1-button");
+    const slider2Button = document.getElementById("slider2-button");
+    const slider3Button = document.getElementById("slider3-button");
+
+    if (slider1Button) slider1Button.addEventListener("click", handleScroll);
+    if (slider2Button) slider2Button.addEventListener("click", handleScroll);
+    if (slider3Button) slider3Button.addEventListener("click", handleScroll);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      if (slider1Button)
+        slider1Button.removeEventListener("click", handleScroll);
+      if (slider2Button)
+        slider2Button.removeEventListener("click", handleScroll);
+      if (slider3Button)
+        slider3Button.removeEventListener("click", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="h-[85vh] w-[100%]  md:h-[85vh]  animate-fade-in-down  mx-auto">
       <Swiper
@@ -31,70 +56,63 @@ export default function App() {
         <SwiperSlide>
           <div className="fondo4 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8   ">
             <div className="absolute inset-0 bg-black bg-opacity-50 " />
-            <div className="w-[80%] md:w-[65%] lg:w-[700px] xl:w-[700px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 rounded-xl p-5 z-10 animate-bounce-fade-in">
-              <h3 className="text-2xl text-white text-center">
+            <div className="w-[80%] md:w-[65%] lg:w-[1000px] xl:w-[1400px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 rounded-xl p-5 z-10 animate-bounce-fade-in">
+              <h3 className="md:text-8xl  text-7xl text-white text-center font-semibold">
                 Tu auto, más que seguro
               </h3>
-              <h1 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-white text-center ">
+              <h1 className="text-2xl text-white text-center ">
                 Viaja con la tranquilidad de tener la mejor cobertura.
               </h1>
 
-              <button className=" w-48 md:w-[30%] px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop">
+              <a
+                id="slider1-button"
+                href="#contact"
+                className="w-48  px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop text-center"
+              >
                 COTIZÁ AHORA
-              </button>
+              </a>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="fondo1 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8   ">
             <div className="absolute inset-0 bg-black bg-opacity-50 " />
-            <div className="w-[80%] md:w-[65%] lg:w-[700px] xl:w-[700px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0  rounded-xl p-5  z-10">
-              <h3 className="text-2xl text-white text-center">
+            <div className="w-[80%] md:w-[65%] lg:w-[1000px] xl:w-[1400px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 rounded-xl p-5 z-10 animate-bounce-fade-in">
+              <h3 className="md:text-7xl  text-5xl text-white text-center font-semibold">
                 Protege la tranquilidad de tu hogar
               </h3>
-              <h1 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-white text-center ">
+              <h1 className="text-2xl text-white text-center ">
                 Combina las coberturas que necesitas para cuidar lo que más te
                 importa.
               </h1>
 
-              <button className=" w-48 md:w-[30%] px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop">
+              <a
+                id="slider2-button"
+                href="#contact"
+                className="w-48  px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop text-center"
+              >
                 COTIZÁ AHORA
-              </button>
+              </a>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="fondo3 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8   ">
             <div className="absolute inset-0 bg-black bg-opacity-50 " />
-            <div className="w-[80%] md:w-[65%] lg:w-[700px] xl:w-[700px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0  rounded-xl p-5  z-10">
-              <h3 className="text-2xl text-white text-center">
+            <div className="w-[80%] md:w-[65%] lg:w-[1000px] xl:w-[1400px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0 rounded-xl p-5 z-10 animate-bounce-fade-in">
+              <h3 className="md:text-8xl  text-7xl text-white text-center font-semibold">
                 Cuidamos tu esfuerzo
               </h3>
-              <h1 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-white text-center ">
+              <h1 className="text-2xl text-white text-center ">
                 Soluciones a medida para que no tengas que preocuparte
               </h1>
-
-              <button className=" w-48 md:w-[30%] px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop">
+              <a
+                id="slider3-button"
+                href="#contact"
+                className="w-48  px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop text-center"
+              >
                 COTIZÁ AHORA
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="fondo2 parallax image relative flex flex-col md:flex-row items-center justify-center  md:h-[100%] my-auto gap-8   ">
-            <div className="absolute inset-0 bg-black bg-opacity-50 " />
-            <div className="w-[80%] md:w-[65%] lg:w-[700px] xl:w-[700px] flex flex-col gap-10 justify-center mx-auto  sm:mx-0  rounded-xl p-5  z-10">
-              <h3 className="text-2xl text-white text-center">
-                Cuidamos tu esfuerzo
-              </h3>
-              <h1 className="text-4xl md:text-4xl lg:text-5xl font-semibold text-white text-center ">
-                Soluciones a medida para que no tengas que preocuparte
-              </h1>
-
-              <button className=" w-48 md:w-[30%] px-[2rem] bg-white text-lg p-3 text-primary mx-auto font-semibold hover:bg-primary hover:text-white rounded-md hover:animate-pop">
-                COTIZÁ AHORA
-              </button>
+              </a>
             </div>
           </div>
         </SwiperSlide>
